73085f61ccd525e362897ae0a58db7171aa5324a
Julian Holfeld <julian@uni-kassel.de>
2022-01-13 11:40:51 +0100

    feat: parse date times

--- code/trend-graph.R
+++ code/trend-graph.R
@@ -1,6 +1,9 @@
 if (!require('pacman')) install.packages('pacman')
-p_load(pacman, tidyverse, ggplot2)
+p_load(pacman, tidyverse, ggplot2, lubridate)
 
-df <- read.csv('data/amulet.csv', header=FALSE)
+df <- read.csv('data/amulet.csv', header = FALSE)
 names(df)[1] <- 'Game'
 names(df)[2] <- 'Date'
+
+# e.g. Mon, 22 Jan 2018 07:37:15 GMT
+dates <- parse_date_time(df$Date, orders = 'dbYHMS', tz = 'GMT')
