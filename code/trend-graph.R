23d5b9fb6d3d2f5d9f660cb35c2be321ae294e81
Julian Holfeld <julian@uni-kassel.de>
2022-01-24 09:30:02 +0100

    feat: floor by month and plot

--- code/trend-graph.R
+++ code/trend-graph.R
@@ -1,5 +1,5 @@
 if (!require('pacman')) install.packages('pacman')
-p_load(pacman, tidyverse, ggplot2, lubridate)
+p_load(pacman, tidyverse, ggplot2, lubridate, plyr)
 
 df <- read.csv('data/amulet.csv', header = FALSE)
 names(df)[1] <- 'Game'
@@ -7,3 +7,7 @@ names(df)[2] <- 'Date'
 
 # e.g. Mon, 22 Jan 2018 07:37:15 GMT
 timestamps <- parse_date_time(df$Date, orders = 'dbYHMS', tz = 'GMT')
+
+ggplot(df) +
+  aes(x = floor_date(timestamps, "month")) +
+  geom_bar()
