f2a0e07fcf0fac7ed5345fe03853851a8e2e23f1
Julian Holfeld <julian@uni-kassel.de>
2022-09-02 10:59:43 +0200

    refactor: csv_to_df function

--- code/trend-graph.R
+++ code/trend-graph.R
@@ -1,20 +1,25 @@
 if (!require('pacman')) install.packages('pacman')
 p_load(pacman, tidyverse, ggplot2, lubridate, plyr)
 
-df <- read.csv('data/amulet.csv', header = FALSE)
-names(df)[1] <- 'Game'
-names(df)[2] <- 'Date'
+csv_to_df <- function(file_name) {
+  df <- read.csv(file_name, header = FALSE)
+  names(df)[1] <- 'Game'
+  names(df)[2] <- 'Date'
 
-# e.g. Mon, 22 Jan 2018 07:37:15 GMT
-timestamps <- parse_date_time(df$Date, orders = 'dbYHMS', tz = 'GMT')
+  # e.g. Mon, 22 Jan 2018 07:37:15 GMT
+  timestamps <- parse_date_time(df$Date, orders = 'dbYHMS', tz = 'GMT')
 
-data <- table(floor_date(timestamps, "month"))
-data <- data.frame(data)
-names(data)[1] <- 'Date'
-names(data)[2] <- 'Count'
+  data <- table(floor_date(timestamps, "month"))
+  data <- data.frame(data)
+  names(data)[1] <- 'Date'
+  names(data)[2] <- 'Count'
 
-data$Count <- cumsum(data$Count)
+  data$Count <- cumsum(data$Count)
+  return(data)
+}
 
-ggplot(data = data, aes(x = Date, y = Count, group = 1)) +
+amulet <- csv_to_df('data/amulet.csv')
+
+ggplot(data = amulet, aes(x = Date, y = Count, group = 1)) +
   geom_line()
 
